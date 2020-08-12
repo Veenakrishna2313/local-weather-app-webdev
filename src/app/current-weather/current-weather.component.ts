46c76b661ba7eb030b3220948bc69667acfc2c77
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2020-08-12 00:25:32 -0700

    Interface and Binding

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {ICurrentWeather} from '../icurrent-weather';
+import { using } from 'rxjs';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  current:ICurrentWeather
+  constructor() {
+this.current={
+  city:'Redmond',
+  country:'US',
+  date:new Date(),
+  image:'',
+  temperature:59,
+  description:'rainy'
+
+}
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+}
