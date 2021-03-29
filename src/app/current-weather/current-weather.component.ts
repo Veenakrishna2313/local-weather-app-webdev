7b12155643c8ece0a12028c94b658dd060cbeb19
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2021-03-28 21:47:09 -0700

    Current weather component complete

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ICurrentWeather} from '../icurrent-weather';
 import { using } from 'rxjs';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-current-weather',
@@ -10,20 +11,13 @@ import { using } from 'rxjs';
 export class CurrentWeatherComponent implements OnInit {
 
   current:ICurrentWeather
-  constructor() {
-  this.current={
-  city:'Redmond',
-  country:'US',
-  date:new Date(),
-  image:'',
-  temperature:59,
-  description:'rainy'
-
-}
-    
+  constructor(private weatherService: WeatherService) {
+      
    }
 
   ngOnInit(): void {
+
+    this.weatherService.getCurrentWeather('Redmond', 'US').subscribe(data=>this.current=data)
   }
 
 }
