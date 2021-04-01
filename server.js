91b0279f8b2dfb29ca1aae154e2d9be3431472b9
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2021-03-31 21:32:58 -0700

    first deploy

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + "/dist"));
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + "/dist/local-weather-app"));
 
+app.get("*", function (req, res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + "/dist/local-weather-app/index.html"));
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
